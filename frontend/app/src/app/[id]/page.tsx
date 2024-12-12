11564f512704570b8e06b987f390a550e167104a
Melisako <120452771+Melisako@users.noreply.github.com>
2024-12-12 20:43:48 +0100

    page-updates

---
+++ frontend/app/src/app/[id]/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useRouter, useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+interface Booking {
+	id: string;
+	date: Date;
+	start_time: string;
+	doctor_name: string;
+	service: string;
+	end_time: string;
+}
+
+const fetchBooking = async (id: string | string[]) => {
+	const response = await fetch(`http://host.docker.internal:5000/api/bookings/${id}`, { cache: 'no-store' });
+	if (!response.ok) {
+		throw new Error('Failed to fetch booking');
+	}
+
+	const data = await response.json();
+	return data;
+};
+
+const BookingPage: React.FC = () => {
+	const router = useRouter();
+	const { id } = useParams();
+
+	const [booking, setBooking] = useState<Booking>(null);
+
+	const handleBack = () => {
+		router.back();
+	};
+
+	useEffect(() => {
+		if (id) {
+			fetchBooking(id).then(res => {
+				setBooking(res);
+			})
+		}
+	}, [id]);
+
+	return (
+		<>
+			{booking ? (
+				<div className="h-[90vh] w-[100%] flex items-center justify-center p-4">
+					<div className="space-y-2 p-8 bg-slate-50 border border-opacity-60 border-slate-500 rounded">
+						<h1 className="text-2xl my-4">Booking Details</h1>
+						<p>Doctor: <b>{booking.doctor_name}</b></p>
+						<p>Service: <b>{booking.service}</b></p>
+						<p>Start Time: <b>{booking.start_time}</b></p>
+						<p>End Time: <b>{booking.end_time}</b></p>
+					</div>
+
+					<button onClick={handleBack} className="fixed top-12 left-24 bg-blue-950 text-white p-4 rounded hover:bg-blue-900 cursor-pointer">
+						Go Back
+					</button>
+				</div>
+			) : (
+				<div className="h-[90vh] w-[100%] flex items-center justify-center p-4">
+					Loading...
+				</div>
+			)}
+		</>
+	);
+};
+
+export default BookingPage;
---
+++ frontend/app/src/app/book/page.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import FormInput from '@/components/FormInput';
+
+interface FormData {
+    service: string;
+    doctor_name: string;
+    start_time: string;
+    end_time: string;
+    date: string;
+};
+
+const BookingsPage = () => {
+    const router = useRouter();
+
+    const [formData, setFormData] = useState<FormData>({
+        service: '',
+        doctor_name: '',
+        start_time: '',
+        end_time: '',
+        date: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleBack = () => {
+        router.back();
+    };
+
+    const convertHourFormat = (time: string): string => {
+        const [hours, minutes] = time.split(':').map(Number);
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        const formattedHours = (hours % 12 || 12).toString().padStart(2, '0');
+        const formattedMinutes = minutes.toString().padStart(2, '0');
+        return `${formattedHours}:${formattedMinutes} ${ampm}`;
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const startTimeFormatted = convertHourFormat(formData.start_time);
+        const endTimeFormatted = convertHourFormat(formData.end_time);
+
+        const updatedFormData = {
+            ...formData,
+            start_time: startTimeFormatted,
+            end_time: endTimeFormatted,
+        };
+
+        try {
+            const response = await fetch('http://host.docker.internal:5000/api/bookings', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updatedFormData),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                toast.error(errorData.message || 'Error inserting booking');
+                throw new Error(errorData.message || 'Error inserting booking');
+            }
+
+            toast.success('Booking inserted successfully');
+            router.push('/');
+        } catch (err) {
+            toast.error(err.message || 'Error inserting booking');
+        }
+    };
+
+    return (
+        <div className="h-[90vh] w-[100%] flex flex-col items-center justify-center p-4">
+            <h1 className='text-2xl my-4'>Make a Booking</h1>
+            <form onSubmit={handleSubmit} className='min-w-min space-y-2 flex flex-col items-center'>
+                <FormInput
+                    type="text"
+                    name="service"
+                    text="Service: "
+                    value={formData.service}
+                    onChange={handleChange}
+                />
+                <FormInput
+                    type="text"
+                    name="doctor_name"
+                    text="Doctor's Name: "
+                    value={formData.doctor_name}
+                    onChange={handleChange}
+                />
+                <FormInput
+                    type="time"
+                    name="start_time"
+                    text="Start Time: "
+                    value={formData.start_time}
+                    onChange={handleChange}
+                />
+                <FormInput
+                    type="time"
+                    name="end_time"
+                    text="End Time: "
+                    value={formData.end_time}
+                    onChange={handleChange}
+                />
+                <FormInput
+                    type="date"
+                    name="date"
+                    text="Date: "
+                    value={formData.date}
+                    onChange={handleChange}
+                />
+                <button type="submit" className='bg-green-900 text-white py-2 px-6 rounded-sm w-fit hover:bg-green-800 cursor-pointer transition-all duration-150'>Submit</button>
+            </form>
+
+            <button onClick={handleBack} className="fixed top-12 left-24 bg-blue-950 text-white p-4 rounded hover:bg-blue-900 cursor-pointer">
+                Go Back
+            </button>
+        </div>
+    );
+};
+
+export default BookingsPage;
